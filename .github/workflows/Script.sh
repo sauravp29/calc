7adf1d9b93eb720803dfccc0e1df4e25164b0fe9
sauravp29 <99320160+sauravp29@users.noreply.github.com>
2022-10-18 18:05:38 +0530

    Delete .github/workflows directory

--- .github/workflows/Script.sh
+++
@@ -1,6 +0,0 @@
-sudo apt update
-sudo apt install ruby-full -y
-sudo apt install wget
-wget https://aws-codedeploy-us-east-1.s3.us-east-1.amazonaws.com/latest/install
-chmod +x ./install
-sudo ./install auto
